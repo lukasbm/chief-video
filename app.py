bb844a15cf0b4b53f58545008308684c8f2050c6
Lukas Böhm <lukas.l.boehm@fau.de>
2021-02-04 00:31:04 +0100

    kind of works

--- app.py
+++ app.py
@@ -1,8 +1,36 @@
-from flask import Flask
+import os
+from datetime import datetime
+from flask import Flask, render_template, abort
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 app = Flask(__name__)
 
+video_path = os.getenv('VIDEO_LOCATION') or '/media/lukas/Share/Videos'
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def dashboard():
+    struc = {}
+    faecher = os.listdir(video_path)
+    for fach in faecher:
+        struc[fach] = []
+        for vid in os.listdir(os.path.join(video_path, fach)):
+            p = os.path.join(video_path, fach, vid)
+            struc[fach].append({
+                'date': datetime.fromtimestamp(os.path.getctime(p)),
+                'size': os.path.getsize(p),
+                'name': vid
+            })
+
+    return render_template('Dashboard.html', files=struc)
+
+
+@app.route('/<string:fach>/<string:name>')
+def video(fach, name):
+    if os.path.exists(os.path.join(video_path, fach, name)):
+        return render_template('Video.html', fach=fach, name=name)
+    else:
+        abort(404)
