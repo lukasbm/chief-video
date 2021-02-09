52c517b8e7e08b7ff08505cc5a02bd3c927081e2
Lukas Böhm <lukas.l.boehm@fau.de>
2021-02-09 18:03:07 +0100

    fehlerbehandlung and speedslider
    now doesent do stuff if it isnt a folder in the top level, or a file in the second level

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ video_path = os.getenv('VIDEO_LOCATION') or '/home/chief/chief-video/videos'
 @app.route('/')
 def dashboard():
     struc = {}
-    faecher = os.listdir(video_path)
-    for fach in faecher:
-        struc[fach] = []
-        for vid in os.listdir(os.path.join(video_path, fach)):
-            p = os.path.join(video_path, fach, vid)
-            struc[fach].append({
-                'date': datetime.fromtimestamp(os.path.getctime(p)),
-                'size': os.path.getsize(p),
-                'name': os.path.splitext(vid)[0],
-                'file': vid
+    for fach in os.scandir(video_path):
+        if not fach.is_dir(): continue
+        struc[fach.name] = []
+        for vid in os.scandir(fach.path):
+            if not vid.is_file(): continue
+            struc[fach.name].append({
+                'date': datetime.fromtimestamp(os.path.getctime(vid.path)),
+                'size': os.path.getsize(vid.path),
+                'name': os.path.splitext(vid.name)[0],
+                'file': vid.name
             })
 
     return render_template('Dashboard.html', files=struc)
@@ -31,7 +31,7 @@ def dashboard():
 @app.route('/<string:fach>/<string:vid>')
 def video(fach, vid):
     p = os.path.join(video_path, fach, vid)
-    if os.path.exists(p):
+    if os.path.exists(p) and os.path.isfile(p):
         return render_template('Video.html', vid={
             'date': datetime.fromtimestamp(os.path.getctime(p)),
             'size': os.path.getsize(p),
@@ -41,3 +41,9 @@ def video(fach, vid):
         })
     else:
         abort(404)
+
+
+@app.route('/video/<string:fach>/<string:vid>')
+def video_file(fach, vid):
+    from flask import send_from_directory
+    return send_from_directory(directory=os.path.join(video_path, fach), filename=vid)
