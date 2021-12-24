b6c91f76415325314a389236d50b37c30376df6c
Lukas <boehm-lukas@outlook.de>
2021-12-24 03:22:17 +0100

    :passport_control: keep working on flask login

--- app.py
+++ app.py
@@ -1,31 +1,71 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, abort, redirect, flash
+from flask import Flask, render_template, abort, redirect, flash, url_for
 from dotenv import load_dotenv
+from flask_login.utils import login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import PasswordField, SubmitField
 from wtforms.validators import DataRequired
+from flask_login import LoginManager, UserMixin, login_user, current_user
+
+
+######################
+######## SETUP #######
+######################
+
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-video_path = os.getenv('VIDEO_LOCATION') or './videos'
-video_url = os.getenv('VIDEO_URL') or '/videos'
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message = "Bitte anmelden"
+
+VIDEO_PATH = os.getenv('VIDEO_LOCATION') or './videos'
+VIDEO_URL = os.getenv('VIDEO_URL') or '/videos'
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+
+######################
+######## UTIL ########
+######################
+
+
+class User(UserMixin):
+    id = 1
+    authenticated = False
+
+    @property
+    def is_authenticated(self):
+        return self.authenticated
+
+
+# user = User()
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User()
 
 
 class LoginForm(FlaskForm):
-    password = PasswordField(label="Passwort eingeben", validators=[DataRequired()])
+    password = PasswordField(label="Passwort eingeben",
+                             validators=[DataRequired()])
     submit = SubmitField(label="Einloggen")
+
+######################
+####### ROUTES #######
+######################
 
 
 @app.route('/')
+@login_required
 def dashboard():
     struc = {}
-    for fach in os.scandir(video_path):
+    for fach in os.scandir(VIDEO_PATH):
         if not fach.is_dir():
             continue
 
@@ -47,8 +87,9 @@ def dashboard():
 
 
 @app.route('/<string:fach>/<string:vid>')
+@login_required
 def video(fach, vid):
-    p = os.path.join(video_path, fach, vid)
+    p = os.path.join(VIDEO_PATH, fach, vid)
     if os.path.exists(p) and os.path.isfile(p):
         return render_template('Video.html', vid={
             'date': datetime.fromtimestamp(os.path.getctime(p)),
@@ -66,12 +107,25 @@ def video(fach, vid):
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('dashboard')
-    else:
-        flash("Falsches Passwort")
-        return render_template("Login.html", form=form)
+        user = try_login(form.password.data)
+        if login_user(user, remember=True):
+            flash("Erfolgreich angemeldet")
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Falsches Passwort")
+    return render_template("Login.html", form=form)
 
 
 @app.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for("dashboard"))
+
+
+
+def try_login(password):
+    user = User()
+    if password == PASSWORD:
+        user.is_authenticated = True
+    return user
